19124eb59e38716484d794f8f80fc318f4d3c678
SHKim <57246360+shkim0824@users.noreply.github.com>
2023-07-18 13:51:41 +0900

    Update MLP.py

--- models/MLP.py
+++ models/MLP.py
@@ -2,3 +2,17 @@ import jax
 import jax.numpy as jnp
 import flax
 from flax import linen as nn
+
+class MLP(nn.Module):
+    features: Sequence[int]
+
+    @nn.compact
+    def __call__(self, inputs, training:bool): # training flag determine usage of moving average or not
+        x = inputs
+        for i, feat in enumerate(self.features):
+            x = nn.Dense(feat)(x)
+            x = nn.BatchNorm(use_running_average=not training)(x)
+            if i != len(self.features) - 1:
+                x = nn.relu(x)
+                x = nn.Dropout(rate=0.1, deterministic=not training)(x)
+        return x
